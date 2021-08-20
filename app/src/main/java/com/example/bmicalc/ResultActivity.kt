85cc58c1a64bda9def3ead39579f9a302856db1d
psw <parksw125268@koreasoft.co.kr>
2021-08-20 22:04:04 +0900

    210820

---
+++ app/src/main/java/com/example/bmicalc/ResultActivity.kt
@@ -0,0 +1,12 @@
+package com.example.bmicalc
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ResultActivity: AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
